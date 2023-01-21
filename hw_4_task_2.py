27275644e1822f32ef300f5b19e0b7e210480f78
Konstantin <rc.work.yarm@gmail.com>
2023-01-21 20:33:02 +0200

    Second task is finished

---
+++ hw_4_task_2.py
@@ -0,0 +1,12 @@
+# 2
+# You have the number 2 as a variable. Multiply it by 2 in two ways.
+# Accordingly, you need to divide it in 2 different ways by 2. number = 2
+
+number = 2
+
+# Multiply
+print(number * 2)
+print(number << 1)
+# Divide
+print(int(number / 2))
+print(number >> 1)
