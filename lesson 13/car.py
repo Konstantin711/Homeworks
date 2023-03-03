c64c844feba2172df35a2b1c18b18c8f33b5185d
Konstantin <rc.work.yarm@gmail.com>
2023-03-03 23:28:07 +0200

    Homework 13 work is in progress

---
+++ lesson 13/car.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+# Encapsulation ^
+
+
+# inheritance
+class Car(ABC):
+
+    # abstraction
+    @abstractmethod
+    def start_engine(self): ...
+
+    def drive(self): ...
+
+    @abstractmethod
+    def stop_engine(self): ...
+
+    @abstractmethod
+    def turn_left(self): ...
+
+    @abstractmethod
+    def turn_right(self): ...
+
+    @abstractmethod
+    def drive_ahead(self): ...
+
+    @abstractmethod
+    def get_gas_level(self): ...
+
+    @abstractmethod
+    def get_car_mileage(self): ...
---
+++ lesson 13/my_car.py
@@ -0,0 +1,34 @@
+from car import Car
+# Encapsulation ^
+
+
+# inheritance
+class MyCar(Car):
+
+    def __init__(self, car_brand):
+
+
+    def start_engine(self):
+        pass
+
+    def stop_engine(self):
+        pass
+
+    def turn_left(self):
+        pass
+
+    def turn_right(self):
+        pass
+
+    def drive_ahead(self):
+        pass
+
+    def get_gas_level(self):
+        pass
+
+    def get_car_mileage(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
